ac54a65f9b64e0ec9b9aa8c0417b3433b74a3ae4
Iliyan <ili.2@abv.bg>
2021-11-17 14:18:51 +0200

    First_commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-
+  var product = document.getElementsByClassName("product");
+  var price = document.getElementsByClassName("price");
+  product.setAttribute("date-price", price.innerText);
 });
