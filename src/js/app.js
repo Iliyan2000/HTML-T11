fda61e8598ec179c11abe9a4171ea62486c8bdf9
Iliyan <ili.2@abv.bg>
2021-11-17 16:03:38 +0200

    Seventh_commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  var product = document.querySelectorAll('.product');
-  var price = document.querySelector('.price');
-  product.dataset.price = price.innerText;
+  var _product = document.querySelectorAll('.product');
+  var _price = document.querySelector('.price');
+  _product.dataset.price = _price.innerText;
 });
