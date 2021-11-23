a985b6ddd200fb2a1ed735423b550d62dc6eba7a
Iliyan <ili.2@abv.bg>
2021-11-23 11:45:18 +0200

    Next_commit

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,9 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   var _product = document.getElementsByClassName("product");
   var _price = document.getElementsByClassName("price");
-  var att = document.createAttribute("data-price");
-  att.value = _price.innerText;
-  _product.setAttributeNode(att);
-  //_product.setAttribute("data-price", _price.innerText); - not working
+  //var att = document.createAttribute("data-price");
+  //att.value = _price.innerText;
+  //_product.setAttributeNode(att);
+  _product[0].setAttribute("data-price", _price.innerText);
   //_product.dataset.price = _price.innerText; - not working
 });
